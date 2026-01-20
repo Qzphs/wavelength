b0ce3c7f290fd6a304762592fe5fceeaed72f323
Qzphs <86579079+Qzphs@users.noreply.github.com>
2026-01-20 20:35:50 +1300

    Add safeguards to bot
    - add a channel whitelist; if channel.txt has a channel id, then restrict bot to only that channel
    - remove unnecessary command prefix
    - add descriptions to commands

--- bot.py
+++ bot.py
@@ -11,11 +11,18 @@ parser.add_argument("-s", "--sync", action="store_true")
 args = vars(parser.parse_args())
 
 
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix="/")
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix=[])
+
+channel_whitelist: list[discord.TextChannel] = []
 
 
 @bot.event
 async def on_ready():
+    try:
+        with open("channel.txt") as file:
+            channel_whitelist.append(bot.get_channel(int(file.read())))
+    except (FileNotFoundError, ValueError):
+        pass
     if args["sync"]:
 
         # TODO remove once this is no longer needed
@@ -28,8 +35,11 @@ async def on_ready():
 
 @bot.tree.command(
     name="reroll",
+    description="Send a random word and a (spoilered) number from 0 to 100.",
 )
 async def reroll(interaction: discord.Interaction):
+    if channel_whitelist and interaction.channel not in channel_whitelist:
+        return
     await interaction.response.send_message(
         f"word: {random_word()}, number: ||`{random_number():>3}`||"
     )
@@ -37,8 +47,11 @@ async def reroll(interaction: discord.Interaction):
 
 @bot.tree.command(
     name="quit",
+    description="Stop running the bot.",
 )
 async def quit(interaction: discord.Interaction):
+    if channel_whitelist and interaction.channel not in channel_whitelist:
+        return
     await interaction.response.send_message(":zany_face:")
     await bot.close()
 
