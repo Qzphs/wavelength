b38e38368dab975cdc89074eb210bf74cb122c87
Qzphs <86579079+Qzphs@users.noreply.github.com>
2026-01-31 14:21:12 +1300

    Add Round class

--- game/__init__.py
+++ game/__init__.py
@@ -1,1 +1,2 @@
+from game.round import Player, Round
 from game.words import random_number, random_word
---
+++ game/round.py
@@ -0,0 +1,27 @@
+from game.words import random_number, random_word
+
+
+Player = str
+
+
+class Round:
+
+    def __init__(self, host: Player, word: str, number: int):
+        self.host = host
+        self.word = word
+        self.number = number
+        self.guesses: dict[Player, int] = {}
+        self.scores: dict[Player, int] = {}
+
+    @classmethod
+    def random(self, host: Player):
+        return Round(host, random_word(), random_number())
+
+    def guess(self, player: Player, number: int):
+        if player == self.host:
+            raise Exception("host cannot guess")
+        self.guesses[player] = number
+        self.scores[player] = abs(self.number - number)
+        self.scores[self.host] = sum(
+            abs(self.number - number) for number in self.guesses.values()
+        )
